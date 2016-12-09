cec4be7fa4bf4d679f92281b4cc6d6a48a00669d
BlakeABos <blakbos811@ed.amdsb.ca>
2016-12-09 12:51:29 -0500

    Add project files.

---
+++ DeathStarBlake/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace DeathStarBlake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.transmissionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // transmissionLabel
+            // 
+            this.transmissionLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.transmissionLabel.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.transmissionLabel.ForeColor = System.Drawing.Color.Black;
+            this.transmissionLabel.Location = new System.Drawing.Point(117, 280);
+            this.transmissionLabel.Name = "transmissionLabel";
+            this.transmissionLabel.Size = new System.Drawing.Size(305, 53);
+            this.transmissionLabel.TabIndex = 1;
+            this.transmissionLabel.Text = "   Transmission incoming! Click anywhere to continue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(529, 422);
+            this.Controls.Add(this.transmissionLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Blow Up The Death Star";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label transmissionLabel;
+    }
+}
+
---
+++ DeathStarBlake/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace DeathStarBlake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = null;
+            transmissionLabel.Visible = false;
+            Refresh();
+
+            Graphics fg = this.CreateGraphics();
+
+            Pen whitePen = new Pen(Color.White, 1);
+            Pen blackPen = new Pen(Color.Black, 4);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            fg.Clear(Color.Black);
+            Font drawFont = new Font("Arial", 14, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Green);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            fg.DrawString("Fly over the death star and drop a bomb to explode it.\nGood Luck Rookie!", drawFont, drawBrush, 0, 0);
+            Thread.Sleep(5000);
+            fg.Clear(Color.Black);
+            fg.DrawEllipse(whitePen, 90, 70, 350, 350);
+            fg.DrawPie(whitePen, 215, 55, 100, 100, 218, 105);
+            fg.DrawPie(blackPen, 215, 50, 100, 100, 218, 105);
+            fg.FillPie(blackBrush, 215, 52, 100, 100, 218, 105);
+            fg.DrawLine(whitePen, 260, 102, 260, 260);
+            fg.DrawLine(whitePen, 270, 102, 270, 260);
+
+            SoundPlayer player2 = new SoundPlayer(Properties.Resources.X_wing_Sound_Effects);
+            player2.Play();
+            for (int x = 0, y = 15; x <= 260; x++)
+            {
+                //act
+                fg.Clear(Color.Black);
+
+
+                fg.FillRectangle(whiteBrush, x, y, 10, 10);
+                fg.DrawEllipse(whitePen, 90, 70, 350, 350);
+                fg.DrawPie(whitePen, 215, 55, 100, 100, 218, 105);
+                fg.DrawPie(blackPen, 215, 50, 100, 100, 218, 105);
+                fg.FillPie(blackBrush, 215, 52, 100, 100, 218, 105);
+                fg.DrawLine(whitePen, 260, 102, 260, 260);
+                fg.DrawLine(whitePen, 270, 102, 270, 260);
+                Thread.Sleep(10);
+            }
+            for (int x = 260, y = 15; y <= 265; y++)
+            {
+                x = x + 2;
+                fg.Clear(Color.Black);
+
+                fg.FillRectangle(whiteBrush, x, 15, 10, 10);
+                fg.FillEllipse(whiteBrush, 260, y, 10, 10);
+                fg.DrawEllipse(whitePen, 90, 70, 350, 350);
+                fg.DrawPie(whitePen, 215, 55, 100, 100, 218, 105);
+                fg.DrawPie(blackPen, 215, 50, 100, 100, 218, 105);
+                fg.FillPie(blackBrush, 215, 52, 100, 100, 218, 105);
+                fg.DrawLine(whitePen, 260, 102, 260, 260);
+                fg.DrawLine(whitePen, 270, 102, 270, 260);
+                Thread.Sleep(10);
+            }
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Explosion_Death_Star_Star_Wars);
+            player.Play();
+            for (int x = 1; x < 400; x++)
+                {
+                
+                fg.FillEllipse(redBrush, 240 - x/2, 265 - x/2, 50 + x  ,50 + x );
+                
+
+                  Thread.Sleep(10);
+                
+            }
+            
+        }
+    }
+}
